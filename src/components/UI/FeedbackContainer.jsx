0017dc9ddb4811b54090bbab32d5af7fdfba6f69
Ctato1 <dznelashvilitato@gmail.com>
2024-04-05 03:18:37 +0400

    fit: feedback container infos

--- src/components/UI/FeedbackContainer.jsx
+++ src/components/UI/FeedbackContainer.jsx
@@ -1,42 +1,63 @@
-// svg files
+/* eslint-disable */
+// Import SVG files
 import upArrow from "/assets/shared/icon-arrow-up.svg";
-import comments from "/assets/shared/icon-comments.svg";
-// User Feedback Container (INFO)
-function FeedbackContainer() {
+import commentsSvg from "/assets/shared/icon-comments.svg";
+
+// User Feedback Container Component
+function FeedbackContainer({
+  title,
+  description,
+  category,
+  upvotes,
+  comments,
+}) {
+  // Ensure comments array is initialized
+  const allComments = comments || [];
+
+  // Calculate total length of comments and replies
+  const totalLength = allComments.reduce((acc, comment) => {
+      if (comment.replies) {
+          return acc + comment?.replies.length;
+      } else {
+          return acc + comments?.length;
+      }
+  }, 0);
+
+
   return (
     <div>
-      <div
-        className="bg-white  rounded-[10px] justify-between items-center
-           p-[24px] md:p-[28px] lg:px-[32px]
-          flex-col md:flex md:flex-row
-           "
-      >
+      {/* User Feedback Container */}
+      <div className="bg-white rounded-[10px] justify-between items-center p-[24px] md:p-[28px] lg:px-[32px] flex-col md:flex md:flex-row">
         <div className="flex gap-[40px]">
+          {/* Upvote button */}
           <button className="hidden md:flex flex-col items-center gap-[10px] bg-[#F2F4FE] px-[13px] py-[6px] rounded-[10px] h-fit">
             <img src={upArrow} alt="up arrow" />
-            <span className="text-[.8125rem] text-[#3A4374] font-bold">99</span>
+            <span className="text-[.8125rem] text-[#3A4374] font-bold">
+              {upvotes}
+            </span>
           </button>
+          {/* Feedback details */}
           <div>
-            <h1 className="text-[#3A4374] text-[.8125rem]">
-              title
-            </h1>
-            <p>
-                description
-            </p>
+            <h1 className="text-[#3A4374] text-[.8125rem]">{title}</h1>
+            <p>{description}</p>
             <button className="bg-[#F2F4FE] text-[#4661E6] px-[13px] py-[6px] rounded-[10px] mb-[14px]">
-              Feature
+              {category}
             </button>
           </div>
         </div>
 
+        {/* Comments and upvote button for mobile */}
         <div className="md:px-5 flex justify-between">
-          <button className="flex items-center gap-[10px] bg-[#F2F4FE] px-[13px] py-[6px] rounded-[10px] h-fit  md:hidden">
+          <button className="flex items-center gap-[10px] bg-[#F2F4FE] px-[13px] py-[6px] rounded-[10px] h-fit md:hidden">
             <img src={upArrow} alt="up arrow" />
-            <span className="text-[.8125rem] text-[#3A4374] font-bold">99</span>
+            <span className="text-[.8125rem] text-[#3A4374] font-bold">
+              {upvotes}
+            </span>
           </button>
+          {/* Comments count */}
           <div className="flex items-center gap-2">
-            <img src={comments} alt="comments" />
-            <span>4</span>
+            <img src={commentsSvg} alt="comments" />
+            <span>{totalLength}</span>
           </div>
         </div>
       </div>
--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -1,28 +1,64 @@
+/* eslint-disable */
+// React imports
+import { useNavigate, useParams } from "react-router";
+import { useContext, useEffect, useState } from "react";
+import { FeedbackContext } from "../store/feedback-context";
+
+// Reactstrap imports
 import { Col, Container, Row } from "reactstrap";
 
-// svg files
+// SVG files
 import leftArrow from "/assets/shared/icon-arrow-left.svg";
-// components
+
+// Custom component imports
 import FeedbackContainer from "../components/UI/FeedbackContainer";
 
+// Feedback Page Component
 function FeedBack() {
+  // Router parameters
+  const { id } = useParams();
+
+  // Context
+  const { productData } = useContext(FeedbackContext);
+
+  // State for feedback
+  const [feedback, setFeedback] = useState(null);
+
+  // Navigation hook
+  const navigate = useNavigate();
+
+  // Effect hook to fetch and set feedback data
+  useEffect(() => {
+    // Filter product data based on id
+    const filtered = productData.filter((product) => product.id === +id)[0];
+
+    // Redirect to main page if no data found
+    if (!filtered) return navigate("/");
+
+    // Set feedback data to state
+    setFeedback(filtered);
+  }, []);
 
   return (
     <div className="bg-[#F7F8FD] p-6">
       <Container>
         <Row>
+          {/* Header */}
           <header className="flex items-center justify-between mb-[24px]">
+            {/* Navigation */}
             <nav className="flex items-center gap-[15px]">
               <img src={leftArrow} alt="left arrow" />
               <span className="text-[.8125rem]">Go Back</span>
             </nav>
+            {/* Edit Feedback button */}
             <button className="py-[10.5px] px-[16px] bg-[#4661E6] rounded-[10px] text-white text-[.8125rem]">
               Edit Feedback
             </button>
           </header>
         </Row>
         <Row>
-         <FeedbackContainer/>
+          {/* Feedback Container */}
+          <FeedbackContainer {...feedback} />
         </Row>
       </Container>
     </div>
