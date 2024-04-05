e4c3ea8e897057b83d5a24854d23aa03dac6b370
saba <marghanias@gmail.com>
2024-04-05 15:28:34 +0400

    WIP: create function for update data

--- src/pages/EditFeedBack.jsx
+++ src/pages/EditFeedBack.jsx
@@ -1,5 +1,22 @@
+import {FeedbackContext} from "../store/feedback-context";
+import {useParams} from "react-router-dom";
+import {useContext} from "react";
+
 function EditFeedBack() {
-  return <div>EditFeedBack</div>;
+  const {id} = useParams();
+  const {productData, editProductData} = useContext(FeedbackContext);
+  const product = productData.filter(product => product.id === +id);
+  console.log(product);
+
+  return (
+    <div>
+      <button
+        onClick={() => editProductData(id, "Hello World", "start", "suggestion", "adadasdsadsad")}
+      >
+        dasdsadas
+      </button>
+    </div>
+  );
 }
 
 export default EditFeedBack;
