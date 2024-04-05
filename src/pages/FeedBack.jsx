b1ff65834ad7b147299b3c490825ab75f61dbc4c
Ctato1 <dznelashvilitato@gmail.com>
2024-04-05 17:31:38 +0400

    add: comments and reply section

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -38,9 +38,68 @@ function FeedBack() {
     // Set feedback data to state
     setFeedback(filtered);
   }, []);
+  // Comment component
+  const Comment = ({ comment }) => {
+    return (
+      <article className="px-8">
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="flex gap-3 items-center md:gap-8">
+            <img
+              src={comment.user.image}
+              alt={comment.user.name}
+              className="w-10 h-10 rounded-full"
+            />
+            <div className="flex flex-col">
+              <h1 className="m-0 text-title text-[13px] font-bold">
+                {comment.user.name}
+              </h1>
+              <p className="m-0 text-paragraph text-[13px]">
+                @{comment.user.username}
+              </p>
+            </div>
+          </div>
+          <button className="text-[#4661E6] text-[13px] font-semibold">
+            Reply
+          </button>
+        </div>
+        <p className="text-paragraph text-[15px] py-[17px]">
+          {comment.content}
+        </p>
+      </article>
+    );
+  };
+
+  // Reply component
+  const Reply = ({ reply }) => {
+    return (
+      <article className="pl-20 pr-8">
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="flex gap-3 items-center md:gap-8">
+            <img
+              src={reply.user.image}
+              alt={reply.user.name}
+              className="w-10 h-10 rounded-full"
+            />
+            <div className="flex flex-col">
+              <h1 className="m-0 text-title text-[13px] font-bold">
+                {reply.user.name}
+              </h1>
+              <p className="m-0 text-paragraph text-[13px]">
+                @{reply.user.username}
+              </p>
+            </div>
+          </div>
+          <button className="text-[#4661E6] text-[13px] font-semibold">
+            Reply
+          </button>
+        </div>
+        <p className="text-paragraph text-[15px] py-[17px]">{reply.content}</p>
+      </article>
+    );
+  };
 
   return (
-    <div className="bg-[#F7F8FD] p-6">
+    <div className="bg-bodyC p-6 min-h-screen">
       <Container>
         <Row>
           {/* Header */}
@@ -51,7 +110,7 @@ function FeedBack() {
               <span className="text-title">Go Back</span>
             </nav>
             {/* Edit Feedback button */}
-            <button className="py-[10.5px] px-[16px] bg-[#4661E6] rounded-[10px] text-textWhite text-[.8125rem]">
+            <button className="py-[10.5px] px-[16px] bg-addBtn hover:bg-addBtnH rounded-[10px] text-textWhite text-[.8125rem]">
               Edit Feedback
             </button>
           </header>
@@ -59,6 +118,25 @@ function FeedBack() {
         <Row>
           {/* Feedback Container */}
           <FeedbackContainer {...feedback} />
+        </Row>
+        <Row>
+          <main className="my-5 bg-containerBg rounded-lg p-6">
+            <h4 className="text-title text-[18px] font-bold tracking-[0.25px] pl-6">
+              {feedback?.comments?.length} Comments
+            </h4>
+            {feedback?.comments?.map((comment, index) => (
+              <div key={comment.id}>
+                <Comment comment={comment} />
+
+                {/* Render replies */}
+                {comment.replies &&
+                  comment.replies.map((reply) => (
+                    <Reply key={reply.id} reply={reply} />
+                  ))}
+                {index !== feedback?.comments?.length - 1 && <hr />}
+              </div>
+            ))}
+          </main>
         </Row>
       </Container>
     </div>
