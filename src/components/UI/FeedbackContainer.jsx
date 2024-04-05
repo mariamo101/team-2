bea8b518d10bd2c74cbd6c20ab6903af487c8f4c
Ctato1 <dznelashvilitato@gmail.com>
2024-04-05 04:37:29 +0400

    style: add status colors and navigation to edit page with id

--- src/components/UI/FeedbackContainer.jsx
+++ src/components/UI/FeedbackContainer.jsx
@@ -1,43 +1,69 @@
 /* eslint-disable */
+import {useNavigate} from 'react-router-dom'
 // Import SVG files
 import upArrow from "/assets/shared/icon-arrow-up.svg";
 import commentsSvg from "/assets/shared/icon-comments.svg";
 
 // User Feedback Container Component
 function FeedbackContainer({
+  id,
   title,
   description,
   category,
   upvotes,
   comments,
+  status,
+  isRoadMap = false,
 }) {
+  const navigate = useNavigate();
   // Ensure comments array is initialized
   const allComments = comments || [];
 
   // Calculate total length of comments and replies
   const totalLength = allComments.reduce((acc, comment) => {
-      if (comment.replies) {
-          return acc + comment?.replies.length;
-      } else {
-          return acc + comments?.length;
-      }
+    if (comment.replies) {
+      return acc + comment?.replies.length;
+    } else {
+      return acc + comments?.length;
+    }
   }, 0);
 
-
+  // Get Status Colors
+  const getColor =
+    status?.toLowerCase() === "planned"
+      ? "bg-[#F49F85]"
+      : status?.toLowerCase() === "in-progress"
+      ? "bg-[#AD1FEA]"
+      : "bg-[#62BCFA]";
   return (
-    <div>
+    <div
+      className={`flex flex-col gap-[20px] bg-white rounded-[10px] p-[24px] md:p-[28px] lg:px-[32px] relative`}
+    >
       {/* User Feedback Container */}
-      <div className="bg-white rounded-[10px] justify-between items-center p-[24px] md:p-[28px] lg:px-[32px] flex-col md:flex md:flex-row">
+
+      {/* If Roadmap Page */}
+      {isRoadMap && status?.toLowerCase() !== "suggestion" && (
+        <div className="status flex items-center">
+          <div
+            className={`${getColor} absolute w-full h-2 left-0 top-0 rounded-t-full`}
+          />
+          <div className={`w-2 h-2 rounded-full ${getColor}`} />
+          <div className="pl-3 text-[#647196] text-[.8125rem]">{status}</div>
+        </div>
+      )}
+
+      <div className="justify-between items-center md:flex md:flex-row">
         <div className="flex gap-[40px]">
           {/* Upvote button */}
+
           <button className="hidden md:flex flex-col items-center gap-[10px] bg-[#F2F4FE] px-[13px] py-[6px] rounded-[10px] h-fit">
             <img src={upArrow} alt="up arrow" />
             <span className="text-[.8125rem] text-[#3A4374] font-bold">
               {upvotes}
             </span>
           </button>
-          {/* Feedback details */}
-          <div>
+          {/* Feedback details and Redirect to edit page*/}
+          <div className='cursor-pointer' onClick={()=>{navigate(`/edit-feedback/${id}`)}}>
             <h1 className="text-[#3A4374] text-[.8125rem]">{title}</h1>
             <p>{description}</p>
             <button className="bg-[#F2F4FE] text-[#4661E6] px-[13px] py-[6px] rounded-[10px] mb-[14px]">
