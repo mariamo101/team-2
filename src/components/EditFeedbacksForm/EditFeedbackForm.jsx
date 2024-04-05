a173e1be549bbf06cc17c4ce16fec3ce9643998f
saba <marghanias@gmail.com>
2024-04-05 22:35:50 +0400

    fix:git

--- src/components/EditFeedbacksForm/EditFeedbackForm.jsx
+++ src/components/EditFeedbacksForm/EditFeedbackForm.jsx
@@ -1,9 +1,27 @@
 import styles from "./EditFeedbackForm.module.css";
 
-function EditFeedbackForm() {
+import {useForm} from "react-hook-form";
+
+function EditFeedbackForm({product}) {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {
+      title: product.title,
+      category: product.category,
+      description: product.description,
+      status: "planned",
+    },
+  });
+
   return (
     <form className={styles.form}>
-      <fieldset></fieldset>
+      <fieldset>
+        <h1 className={styles.title}>{product.title}</h1>
+      </fieldset>
     </form>
   );
 }
