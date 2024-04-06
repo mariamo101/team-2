197817119c7f63392ce4b29932e27446d8b87120
Ctato1 <dznelashvilitato@gmail.com>
2024-04-06 18:51:01 +0400

    add: mobile navigation

--- src/pages/RoadMap.jsx
+++ src/pages/RoadMap.jsx
@@ -1,17 +1,39 @@
 /* eslint-disable */
-import { Container, Row } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 // Button for feedback
 import MainButton from "../components/feedbackboard/mainButton/MainButton";
-// SV files
-import leftArrow from "/assets/shared/icon-arrow-left.svg";
+import { useState } from "react";
+
+const statuses = [
+  {
+    name: "Planned",
+    id: 1,
+    color: "#F49F85",
+  },
+  {
+    name: "In-Progress",
+    id: 2,
+    color: "#AD1FEA",
+  },
+  {
+    name: "Live",
+    id: 3,
+    color: "#62BCFA",
+  },
+];
 function RoadMap() {
+  const [stateNum, setStateNum] = useState(1);
+  console.log(stateNum);
   return (
     <>
       <Container className="max-w-screen-desktop">
         <Row>
-          <header className="bg-headerBg flex justify-between items-center text-textSecondary px-4 py-4">
-            <div className="flex flex-col items-center justify-center">
-              <div className="flex  items-center gap-3 cursor-pointer flex-wrap">
+          <header className="bg-headerBg flex justify-between items-center text-textSecondary px-4 py-4 sm:rounded-lg">
+            <Col
+              lg="6"
+              className="items-center justify-between text-[.6125rem] mobile:text-[.8125rem] flex flex-col"
+            >
+              <div className="flex  items-center gap-3 cursor-pointer flex-wrap  flex-row self-start">
                 <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
                   <path
                     d="M6 9L2 5l4-4"
@@ -21,13 +43,38 @@ function RoadMap() {
                     fill-rule="evenodd"
                   />
                 </svg>
-                <span className="text-[.8125rem] font-bold">Go Back</span>
+                <span className=" font-bold">Go Back</span>
               </div>
               <p className="m-0 tracking-[-0.25px] font-bold">Roadmap</p>
-            </div>
-
-            <MainButton text="+ Add Feedback" path='/new-feedback'/>
+            </Col>
+            <Col lg="6" className="flex flex-row-reverse">
+              <MainButton text="+ Add Feedback" path="/new-feedback" />
+            </Col>
           </header>
+        </Row>
+        <Row>
+          <div className="flex mobile:hidden">
+            {statuses.map((status) => (
+              <Col
+                key={status.id}
+                xs={12 / statuses.length}
+                className="flex  justify-center items-center mt-3 relative"
+                onClick={() => setStateNum(status.id)}
+              >
+                <span
+                  className={`font-bold text-[13px] mb-2 ${
+                    status.id === stateNum ? "text-title" : "text-viewD"
+                  }`}
+                >
+                  {status.name}
+                </span>
+                <div
+                  className={`w-[60%] h-1 bg-[${status.color}] absolute -bottom-1  rounded-sm`}
+                />
+              </Col>
+            ))}
+            <div className="mt-1" />
+          </div>
         </Row>
       </Container>
     </>
